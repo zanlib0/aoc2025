ef05881c7f6498a664818d036325b189bc3da4b8
zanlib0 <hello@zanlib.dev>
2025-12-01 22:30:28 +0100

    complete day 1 part 1

--- bin/day01.ml
+++ bin/day01.ml
@@ -15,8 +15,6 @@ type instruction =
   | TurnLeft of int
   | TurnRight of int
 
-let dial_starter = 50
-
 let parse_turn instruction =
   Scanf.sscanf instruction "%c%d" (fun dir steps  ->
     match dir with
@@ -25,7 +23,27 @@ let parse_turn instruction =
     | _ -> failwith "Invalid direction"
   )
 
+(* OCaml keeps sign of dividend when doing mod instead of wrapping. *)
+let wrap n =
+  let r = n mod 100 in
+  if r < 0 then r + 100 else r
+
+let step (pos, tally) instruction =
+  let new_pos = 
+    match instruction with
+    | TurnLeft steps -> pos - (steps mod 100)
+    | TurnRight steps -> pos + (steps mod 100)
+    |> wrap in
+  let new_tally = if new_pos = 0 then tally + 1 else tally in
+  (wrap new_pos, new_tally)
+
 let solve input =
   let lines = split_lines input in
   let instructions = List.map parse_turn lines in
-  instructions
+  let (_final_pos, tally) = List.fold_left step (50, 0) instructions in
+  tally
+
+let () =
+  let input = read_input 1 in
+  Printf.printf "Example: %d\n" (solve example_input);
+  Printf.printf "Part 1: %d\n" (solve input)
--- lib/utils.ml
+++ lib/utils.ml
@@ -2,6 +2,6 @@ let read_input day =
   let filename = Printf.sprintf "resources/day%02d.txt" day in
   In_channel.with_open_text filename In_channel.input_all
 
-let split_lines str = String.split_on_char '\n' str
+let split_lines str = String.split_on_char '\n' str |> List.filter (fun s -> s <> "")
 
 let unlines strs = String.concat "\n" strs
