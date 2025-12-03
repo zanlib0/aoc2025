a2fd6f0ce5f90774502d83b68321171fce87b0b7
zanlib0 <hello@zanlib.dev>
2025-12-03 20:03:34 +0100

    refactor day 3

--- bin/day03.ml
+++ bin/day03.ml
@@ -10,28 +10,22 @@ let prep input =
   |> split_lines
   |> List.map (fun line -> line |> String.to_seq |> List.of_seq |> List.map ((String.make 1) >> int_of_string))
 
-let rec find_largest_first_digit_index ?(digit=9) list =
+let rec find_largest_nth_digit_index ?(digit=9) offset list =
   if digit < 0 then invalid_arg "digit must be >= 0";
+  if offset < 0 then invalid_arg "offset must be >= 0";
   let len = List.length list in
   let index = List.find_index (fun el -> el = digit) list in
   let index' = match index with
-    | Some i -> if i = (len - 1) then None else Some i
+    | Some i -> if i = (len - offset) then None else Some i
     | None -> None in
   match index' with
-  | None -> find_largest_first_digit_index ~digit:(digit - 1) list
-  | Some i -> i
-
-let rec find_largest_digit_index ?(digit=9) list =
-  if digit < 0 then invalid_arg "digit must be >= 0";
-  let index = List.find_index (fun el -> el = digit) list in
-  match index with
-  | None -> find_largest_digit_index ~digit:(digit - 1) list
+  | None -> find_largest_nth_digit_index ~digit:(digit - 1) offset list
   | Some i -> i
 
 let find_row_joltage row =
-  let largest_first_digit_index = find_largest_first_digit_index row in
+  let largest_first_digit_index = find_largest_nth_digit_index 1 row in
   let remainder = List.drop (largest_first_digit_index + 1) row in
-  let largest_second_digit_index = find_largest_digit_index remainder in
+  let largest_second_digit_index = find_largest_nth_digit_index 0 remainder in
   let first_digit = List.nth row largest_first_digit_index in
   let second_digit = List.nth remainder largest_second_digit_index in
   int_of_string (string_of_int first_digit ^ string_of_int second_digit)
