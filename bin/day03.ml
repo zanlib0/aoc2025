544418f581edecd7b4e99fc09564b321182ebcf0
zanlib0 <hello@zanlib.dev>
2025-12-03 20:45:37 +0100

    day 3 stage 2

--- bin/day03.ml
+++ bin/day03.ml
@@ -16,7 +16,7 @@ let rec find_largest_nth_digit_index ?(digit=9) offset list =
   let len = List.length list in
   let index = List.find_index (fun el -> el = digit) list in
   let index' = match index with
-    | Some i -> if i = (len - offset) then None else Some i
+    | Some i -> if i >= (len - offset) then None else Some i
     | None -> None in
   match index' with
   | None -> find_largest_nth_digit_index ~digit:(digit - 1) offset list
@@ -30,15 +30,37 @@ let find_row_joltage row =
   let second_digit = List.nth remainder largest_second_digit_index in
   int_of_string (string_of_int first_digit ^ string_of_int second_digit)
 
+let rec find_row_big_joltage_indexes ?(length=12) ?(offset=0) row =
+  if length < 0 then invalid_arg "length must be >= 0";
+  if offset < 0 then invalid_arg "offset must be >= 0";
+  if length = 0 then []
+  else
+    let i = find_largest_nth_digit_index (length - 1) row in
+    let remainder = List.drop (i + 1) row in
+    i + offset :: find_row_big_joltage_indexes ~length:(length - 1) ~offset:(i + offset + 1) remainder
+
+let find_row_big_joltage row =
+  find_row_big_joltage_indexes row
+  |> List.map (List.nth row)
+  |> List.map string_of_int
+  |> List.fold_left (^) ""
+  |> int_of_string
+
 let solve input =
   input
   |> prep
   |> List.map find_row_joltage
   |> List.fold_left (+) 0
 
+let solve' input =
+  input
+  |> prep
+  |> List.map find_row_big_joltage
+  |> List.fold_left (+) 0
+
 let () =
   let input = read_input 3 in
   Printf.printf "Example 1: %d\n" (solve example_input);
   Printf.printf "Part 1: %d\n" (solve input);
-  (* Printf.printf "Example 2: %d\n" (solve' example_input); *)
-  (* Printf.printf "Part 2: %d\n" (solve' input); *)
+  Printf.printf "Example 2: %d\n" (solve' example_input);
+  Printf.printf "Part 2: %d\n" (solve' input);
